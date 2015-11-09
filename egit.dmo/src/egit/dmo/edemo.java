d64c1565d9a4a6486695896400c6b3fc73022a71
FA12-BSE-113 <FA12-BSE-113@LAB7C-04.cns.ciit>
2015-11-10 00:09:33 +0500

    sami mai war mai warri...

---
+++ egit.dmo/src/egit/dmo/edemo.java
@@ -0,0 +1,10 @@
+package egit.dmo;
+
+public class edemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Lets do something using GIT");
+	}
+
+}
